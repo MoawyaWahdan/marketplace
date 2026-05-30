a15cc74bcb0e1fc766c69f0a5ec416c3eb39b26f
MWahdan <wahdan44@gmail.com>
2026-05-29 17:55:35 -0700

    Add psycopg DATABASE_URL otherwise on depolyment psycopg2 is assumed

--- app/core/config.py
+++ app/core/config.py
@@ -16,3 +16,9 @@ LISTINGS_IMAGES_URL_PATH = "/static/images/listing_images"
 DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL is not set in environment variables")
+if DATABASE_URL.startswith("postgresql://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgresql://",
+        "postgresql+psycopg://",
+        1,
+    )
